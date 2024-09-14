d64a5517ea6c0dacbaabf3dfb20367b7b27e580e
KenilAvaiyaa <avaiyakekenil@gmail.com>
2024-09-14 18:48:58 -0400

    look good

--- index.js
+++ index.js
@@ -11,3 +11,39 @@ function raf(time) {
 }
 
 requestAnimationFrame(raf)
+
+gsap.registerPlugin(ScrollTrigger)
+
+document.querySelectorAll(".aspect-square").forEach(elem => {
+  let img = elem.querySelector("img")
+  // console.log(img)
+  let xrandom = gsap.utils.random(-100, 100)
+  let t1 = gsap.timeline({
+
+  })
+  t1
+  .set(img,{
+    transformOrigin:`${xrandom < 0 ? 0 : '100%'}`
+  },"start")
+  .to(img, {
+    scale: 0,
+    ease: "none",
+    scrollTrigger: {
+      trigger: img,
+      start: "top top",
+      end: "bottom top",
+      scrub: 1
+    }
+  },"start")
+  .to(elem,{
+    xPercent: xrandom,
+    ease: "transform 0.6s cubic-bezier(0.45, 0, 0.55, 1)",
+    scrollTrigger: {
+      trigger: img,
+      start: "top bottom",
+      end: "bottom top",
+      scrub: 1
+    }
+  },"start")
+  
+})
